b898b45b90927dd04bb8c8c017c2ba2ef43153da
HauLuong2003 <hauluong0109@gmail.com>
2024-07-12 17:24:45 +0700

    add product

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Diagnostics.Metrics;
 using Web_Ecommerce_Cilent;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+var app = builder.Build();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
