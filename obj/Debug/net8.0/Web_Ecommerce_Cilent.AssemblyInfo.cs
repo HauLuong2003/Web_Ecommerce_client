3ddc9398aad87479597d34d054d7e776ec20c2e7
HauLuong2003 <hauluong0109@gmail.com>
2024-08-03 19:03:00 +0700

    layout home

--- obj/Debug/net8.0/Web_Ecommerce_Cilent.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web_Ecommerce_Cilent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web_Ecommerce_Cilent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b33eb0a7726a4448901070075fbaa2a609ca718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a6a4e28e577c77545890b4fdd1d291c06cb2db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web_Ecommerce_Cilent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web_Ecommerce_Cilent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
